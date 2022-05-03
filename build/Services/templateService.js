afb790e314918cf55c11d20cb44ce432617bd8ed
Bart Rijnders <b.rijnders@student.fontys.nl>
2022-05-03 18:05:55 +0200

    removed build

--- build/Services/templateService.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TemplateService = void 0;
-class TemplateService {
-}
-exports.TemplateService = TemplateService;
